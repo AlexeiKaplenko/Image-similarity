8e10c7132b08d1bad044fdceb7b8884f4813ec52
AlexeiKaplenko <40427300+AlexeiKaplenko@users.noreply.github.com>
2020-06-12 09:18:36 +0200

    Update TripletNet_MINE.py

--- TripletNet_MINE.py
+++ TripletNet_MINE.py
@@ -17,7 +17,7 @@ import matplotlib
 import matplotlib.pyplot as plt
 
 from losses import TripletLoss, TripletLoss_log1p, TripletLoss_64_patches
-from networks import EmbeddingNet_MINE, EmbeddingNet, TripletNet, TripletNet_MINE, MNAS_Net, MNAS_Net_transformed, ClassificationNet, SiameseNet_MINE
+from networks import EmbeddingNet_MINE, TripletNet_MINE, MNAS_Net, MNAS_Net_transformed, SiameseNet_MINE
 from metrics import AccumulatedAccuracyMetric
 
 margin = 1.
